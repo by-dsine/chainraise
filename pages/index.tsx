ed4fb174c9bc2368448d1bbf1fe512398780c483
dsine-eth <dsine@protonmail.com>
2022-08-08 08:10:07 -0700

    added nice styling to signin screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import Header from '../components/navigation/Header';
-import Footer from '../components/navigation/Footer';
 import { ChevronRightIcon } from '@heroicons/react/solid';
 import { GetServerSideProps } from 'next';
-import { prisma } from '../lib/db';
+import Footer from '../components/navigation/Footer';
+import Header from '../components/navigation/Header';
 import { OfferingPreview } from '../types/typings';
 
 const trendingRaises = [
